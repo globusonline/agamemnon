ccd55bdcdf0336fe4671c327494c6fb75c451075
Brendan McCollam <bmccollam@uchicago.edu>
2014-08-22 15:40:21 -0500

    Format before raising

--- agamemnon/delegate.py
+++ agamemnon/delegate.py
@@ -43,5 +43,5 @@ class Delegate(object):
                     return attr
                 except AttributeError:
                     pass
-            raise AttributeError("No plugin has attribute: %s", item)
+            raise AttributeError("No plugin has attribute: ".format(item))
 
